306a01f87aeb3b62c4d9eb0ce6de47748a429349
Rohit Sarkar <rohitsarkar034@gmail.com>
2023-12-30 11:18:40 +0530

    Creating functionalities to enable sellers to add new items. Also creating a simple dashboard for india_Emart. Modifications made since the last push include: 1)Creating a forms.py file in the item app to collect data about the new item to be added. After this, moving to the views.py file in the same directory and making some modifications to create views 2)To require the user to be logged in to add new item, using @login_required on top of the item creation function in the views.py file 3)In templates/item directory, creating the form.html file and also adding the new item’s path to the urls.py file of the item app4)Replacing the new item link with the actual one in the base.html file 5)Checking the validity of the form up for submission and if true, saving it and redirecting the user to the item detail page 6)Creating a new app named dashboard via the terminal and adding it to the settings.py file. Also importing the login decorator in the views.py file of the same directory 7)Creating an index function with a request parameter to get all user-created items and returning the rendered template with specific parameters 8)Creating index.html file in the templates/dashboard directory

--- india_Emart/item/urls.py
+++ india_Emart/item/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'item' #acts as a namespace for this application
 
 urlpatterns = [
+    path('new/', views.new, name='new'),
     path('<int:pk>', views.detail, name='detail'), #expects an int (pk) which has to be same and the ph has to be same as the one in views.py
 ]
