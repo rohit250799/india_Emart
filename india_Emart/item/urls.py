d7f63e120dafa6daca6ee13c090242db1ef342fa
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-01-05 07:37:45 +0530

    Create an item searching functionality for india Emart. Modifications made since the last commit include: 1)In the views.py file of the item app, creating an item function 2) Importing the items path in the urls.py file of the item app 3)Creating a new file named items.html in templates/item directory which extends the core/base.html file 4)For the sidebar on leftside, creating a form with get method to receive the query input by the user and inside the items method of views.py file using a variable to store the query and rendering it in the return method 5)Filtering the search results 6)To search in the description, importing Q in views.py file of the item app 7)In items.html file, using for each loop to display appropriate items and categories 8)To make it possible to filter on the categories, inside the items method of the views.py file of item app, using an if condition with category_id 9)Also, making it possible to clear filters using a button that redirects to the items.html page

--- india_Emart/item/urls.py
+++ india_Emart/item/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'item' #acts as a namespace for this application
 
 urlpatterns = [
+    path('', views.items, name='items'),
     path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'), #expects an int (pk) which has to be same and the ph has to be same as the one in views.py
     path('<int:pk>/delete/', views.delete, name='delete'),
